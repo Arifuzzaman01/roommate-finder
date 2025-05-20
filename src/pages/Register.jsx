127e64687abbc4976037229a7f2b4711ac700a9e
Arifuzaman <arifuzzamanar29@gmail.com>
2025-05-20 12:31:21 +0600

    password validation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,18 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Navbar from "../components/Navbar";
 import GoogleLogin from "../components/GoogleLogin";
 import { Link } from "react-router";
 import { AuthContext } from "../Layout/AuthProvider";
+import Swal from "sweetalert2";
 
 const Register = () => {
   const { createUser, setUser, user, updataUserProfile } =
     useContext(AuthContext);
+  const [validPass, setValidPass] = useState("");
+  const isValidPassword = (password) =>
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{6,}$/.test(password);
   const handleRegister = (e) => {
     e.preventDefault();
+
     const form = e.target;
     const formData = new FormData(form);
     const { name, email, photo, password } = Object.fromEntries(formData);
     //   console.log(name,email,photo,password)
+    if (!isValidPassword(password)) {
+      return setValidPass(
+        "Password must be at least 6 characters and include uppercase, lowercase, number, and special character."
+      );
+    }
+
     createUser(email, password)
       .then((result) => {
         const userData = result.user;
@@ -20,6 +31,13 @@ const Register = () => {
         updataUserProfile({ displayName: name, photoURL: photo })
           .then(() => {
             setUser({ ...userData, displayName: name, photoURL: photo });
+            Swal.fire({
+              position: "top",
+              icon: "success",
+              title: "Your work has been saved",
+              showConfirmButton: false,
+              timer: 1500,
+            });
           })
           .catch((error) => {
             setRegError(error?.message);
@@ -30,7 +48,7 @@ const Register = () => {
         console.log(error);
       });
   };
-  console.log(user);
+  //   console.log(user);
   return (
     <>
       <Navbar></Navbar>
@@ -70,7 +88,7 @@ const Register = () => {
               placeholder="Password"
               required
             />
-
+            <span className="text-red-600">{validPass ? validPass : ""}</span>
             <button type="submit" className="btn btn-neutral mt-4">
               Register
             </button>
